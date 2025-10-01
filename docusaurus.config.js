89213c65641e59cdaf4c7c1f2f01b99f84e9bbd5
Rolyx <rolyx.yao@thetchoumtechnologies.com>
2025-10-01 22:59:59 +0000

    fix: correct GitHub Pages url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
     v4: true,
   },
 
-  url: 'https://github.com/Rolyx/TechTchoumDoc', // ton GitHub Pages
+  url: 'https://Rolyx.github.io', // ton GitHub Pages
   baseUrl: '/TechTchoumDoc/',
 
   organizationName: 'Rolyx', // ✅ juste ton pseudo GitHub
